c5c7590d022361bf02f652deacee55b4f86ce4b8
Dani Riera <drieradev@gmail.com>
2026-03-25 23:34:59 +0100

    chore: remove internal resolveTokens and ButtonVariant from public exports (#7)

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,5 +2,3 @@ export { palettes, getPalette } from './palettes';
 export type { Palette, PaletteKey, PaletteStep } from './palettes';
 export { RizomaProvider, useRizomaTheme } from './RizomaProvider';
 export type { RizomaTheme } from './types';
-export { resolveTokens } from './resolveTokens';
-export type { ButtonVariant } from './resolveTokens';
